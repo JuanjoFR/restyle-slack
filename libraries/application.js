1cae066e30a4d9c00e213a40f2b255d19e8e405b
Juanjo Fernández <juanjo.fernandez@haa.st>
2022-02-28 17:57:02 +0100

    Clean files and configure linter and basic React Navigation

---
+++ libraries/application.js
@@ -0,0 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { Text } from "react-native";
+
+function Application() {
+  return (
+    <NavigationContainer>
+      <Text>hello world</Text>
+    </NavigationContainer>
+  );
+}
+
+export default Application;
