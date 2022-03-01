f7753419c056ef932963df6c908862f1ecdb85e1
Juanjo Fernández <juanjo.fernandez@haa.st>
2022-03-01 13:17:45 +0100

    Improve the main screen layout

--- screens/direct-messages.js
+++ screens/direct-messages.js
@@ -1,6 +1,14 @@
+import { useTheme } from "@shopify/restyle";
 import React from "react";
-import ScreenWrapper from "../style-system/screen-wrapper";
+import { ScrollView, StatusBar, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Text from "../style-system/text";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 /**
  * Renders the direct messages screen.
@@ -8,10 +16,23 @@ import Text from "../style-system/text";
  * @returns {object} Component JSX.
  */
 function DirectMessages() {
+  const theme = useTheme();
+
   return (
-    <ScreenWrapper>
-      <Text variant="header">Direct messages</Text>
-    </ScreenWrapper>
+    <SafeAreaView
+      edges={["right", "left"]}
+      style={[
+        styles.container,
+        {
+          backgroundColor: theme.colors.mainBackground
+        }
+      ]}
+    >
+      <StatusBar barStyle="light-content" />
+      <ScrollView>
+        <Text variant="header">Direct messages</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
--- screens/mentions.js
+++ screens/mentions.js
@@ -1,6 +1,14 @@
+import { useTheme } from "@shopify/restyle";
 import React from "react";
-import ScreenWrapper from "../style-system/screen-wrapper";
+import { ScrollView, StatusBar, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Text from "../style-system/text";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 /**
  * Renders the mentions screen.
@@ -8,10 +16,23 @@ import Text from "../style-system/text";
  * @returns {object} Component JSX.
  */
 function Mentions() {
+  const theme = useTheme();
+
   return (
-    <ScreenWrapper>
-      <Text variant="header">Mentions</Text>
-    </ScreenWrapper>
+    <SafeAreaView
+      edges={["right", "left"]}
+      style={[
+        styles.container,
+        {
+          backgroundColor: theme.colors.mainBackground
+        }
+      ]}
+    >
+      <StatusBar barStyle="light-content" />
+      <ScrollView>
+        <Text variant="header">Mentions</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
